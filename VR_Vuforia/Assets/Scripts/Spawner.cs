5462a1b1d3809fc3dafd306e744f852424c6127b
DiegoBV <diego.baratto8@gmail.com>
2020-02-11 00:32:15 +0100

    initial commit

---
+++ VR_Vuforia/Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Tooltip("Storage and management of gameObjects")]
+    public GameObjectPool gameObjectPool;
+    [Tooltip("Waiting time to spawn a gameObject")]
+    public float timeToSpawn;
+    public GameObject[] spawnPoints;
+
+    private float currentTime = 0;
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime >= timeToSpawn)
+        {
+            // spawn
+            GameObject go = gameObjectPool.GetInactiveGameObject();
+            resetGameObject(go);
+            currentTime = 0;
+        }
+    }
+
+    private void resetGameObject(GameObject go)
+    {
+        Vector3 spawnPos = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+        go.transform.position = new Vector3(spawnPos.x, spawnPos.y, spawnPos.z);
+        go.SetActive(true);
+    }
+}
---
+++ VR_Vuforia/Assets/Scripts/TiltMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class TiltMovement : MonoBehaviour
+{
+    public float speed;
+
+    public Camera cam;
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector3 movement = new Vector3(Input.acceleration.x, 0.0f, 0.0f);
+        rigidBody.velocity = movement * speed;
+    }
+}
